edf85783b8d50950147fceddd1f30f06b0920d3e
Nicholas Keller <nkeller5627@gmail.com>
2024-07-26 23:34:37 -0400

    Day 1, hour 3 (timer)

--- index.ts
+++ index.ts
@@ -1,5 +1,18 @@
 import express from "express";
 const app = express();
+
+interface Project {
+    author: string;
+    github: string;
+}
+
+var currentPrompt = "Build a quiz app";
+var showcase: Project[] = [];
 app.use((req, res, next) => {console.log(`${req.method} ${req.path}`); next()});
 app.use(express.static("public"));
+app.get("/prompt", (req, res) => res.end(currentPrompt));
+app.get("/showcase", (req, res) => {
+    res.json(showcase);
+    res.end();
+})
 app.listen(8080, () => console.log("http://localhost:8080"));
