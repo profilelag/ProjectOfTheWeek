579f2b27f8f50c275f54b63f9f93c14645bc935b
Nicholas Keller <nkeller5627@gmail.com>
2024-07-28 15:30:31 -0400

    Day 3, hour 2

--- public/submit/script.js
+++ public/submit/script.js
@@ -1,13 +1,12 @@
 const code = (new URLSearchParams(window.location.search)).get('code');
 if(!code && !document.cookie.includes("token=")) window.location.href = '/';
-
+var token = document.cookie.split("token=")[1];
 if(code) axios.get(`/genToken?code=${code}`).then(res => {
     const body = res.data
     if(!body) return window.location.href = '/';
     document.cookie = "token="+body+";path=/";
+    window.location.href = '/submit';
 });
-
-const token = document.cookie.split("token=")[1];
 
 axios.get("https://api.github.com/user", {
     headers: {
@@ -16,4 +15,16 @@ axios.get("https://api.github.com/user", {
 })
 .then(res => {
     $("#welcome")[0].innerHTML = `Welcome, ${res.data.name}!`
+    axios.get("https://api.github.com/user/repos", {
+        headers: {
+            "Authorization": `Bearer ${token}`
+        }
+    }).then(repos => {
+        repos.data.filter(x => x.owner.login == res.data.login).forEach(repo => {
+            $("#projects")[0].innerHTML += `<div class="project">${repo.full_name}</div>`
+        })
+    })
+}).catch(err => {
+    console.log(err)
+    window.location.href = '/';
 })
